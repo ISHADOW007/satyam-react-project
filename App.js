d48ba47d99face3b1a69ce98500585f1ca3f2fde
Satyam Govind Rao <satyamgrao007@gmail.com>
2024-05-15 22:05:17 +0530

    message

--- App.js
+++ App.js
@@ -1,14 +1,18 @@
-
+// React.createElement => ReactELement-JS Object => HTMLElement(render)
 import React from "react";
-
 import ReactDOM from "react-dom";
 
-const parent = React.createElement("div",{id:"parent"},
-React.createElement("div",{id:"child"},
-React.createElement("h1",{},"I am satyam")
-
-    
-));
+// const heading = React.createElement(
+//     "h1",
+//     { id: "heading "},
+//     "Namaste React"  
+// );
+// console.log(heading);
+//JSX (transpiled before it reaches the JS) -PARCEL-Babel
+// JSX => React.createElement => ReactElement-JS Object => HTMLElement(render);
+//jsx - HTML-like or XML-like syntax
+const jsxHeading = <h1 id="heading"> Namaste react using JSX </h1>
+console.log(jsxHeading);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(jsxHeading);
